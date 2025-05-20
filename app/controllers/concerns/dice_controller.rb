2bf4fd02421f6c5ecf19b3fccacf08081e8a57a8
saiindirat <tholusaiindira@gmail.com>
2025-05-20 01:54:07 +0000

    my commit13

--- app/controllers/concerns/dice_controller.rb
+++ app/controllers/concerns/dice_controller.rb
@@ -1,8 +1,25 @@
 class DiceController < ApplicationController
-  def two_six
-    @first_die = rand(1..6)
-    @second_die = rand(1..6)
+  def home
+    render({ :template => "dice_templates/home" })
+  end
 
+  def two_six
+    @rolls = [rand(1..6), rand(1..6)]
     render({ :template => "dice_templates/2d6" })
+  end
+
+  def two_ten
+    @rolls = [rand(1..10), rand(1..10)]
+    render({ :template => "dice_templates/2d10" })
+  end
+
+  def one_twenty
+    @rolls = [rand(1..20)]
+    render({ :template => "dice_templates/1d20" })
+  end
+
+  def five_four
+    @rolls = Array.new(5) { rand(1..4) }
+    render({ :template => "dice_templates/5d4" })
   end
 end
